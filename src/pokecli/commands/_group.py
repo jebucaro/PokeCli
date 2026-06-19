94e7e3fce3b1df804c4f8e2b9722e11d15264ca6
Jonathan Bucaro <jebucaro@users.noreply.github.com>
2026-06-19 11:46:18 -0600

    :bug: fix click import by switching to typer.Context in ResourceGroup

--- src/pokecli/commands/_group.py
+++ src/pokecli/commands/_group.py
@@ -1,11 +1,11 @@
-import click
+import typer
 from typer.core import TyperGroup
 
 
 class ResourceGroup(TyperGroup):
     """Allow `pokecli pokemon pikachu` as a shortcut for `pokecli pokemon get pikachu`."""
 
-    def resolve_command(self, ctx: click.Context, args: list[str]):
+    def resolve_command(self, ctx: typer.Context, args: list[str]):
         if args:
             first = args[0]
             if not first.startswith("-") and first not in self.commands:
